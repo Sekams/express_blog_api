077b76d87e14149314ce450c56a5ac5a5500cf18
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2018-02-12 11:55:12 +0300

    Add error handling and versioning

--- app.js
+++ app.js
@@ -7,10 +7,31 @@ var routes = require("./routes");
 var jsonParser = require("body-parser").json;
 var logger = require("morgan");
 
+//Log requests in console using Morgan
 app.use(logger("dev"));
+
+//Parse JSON objects from the requests
 app.use(jsonParser());
 
-app.use("/", routes);
+//Handle HTTP routes
+app.use("/v1", routes);
+
+//Catch 404 and foward to error handler
+app.use(function (req, res, next) {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+//Error handler
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    });
+});
 
 var port = process.env.PORT || 3000;
 
