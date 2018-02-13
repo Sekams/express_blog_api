78cef1475b601909617ebb8f39b4c1c6be6c2e8a
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2018-02-14 00:29:08 +0300

    Add database setup
    - Add models for structuring documents
    - Add Bcrypt library for hashing user password before saving
    - Implement route functionality to CRUD models

---
+++ route/post_routes.js
@@ -0,0 +1,146 @@
+'use strict';
+
+var express = require("express");
+var router = express.Router();
+var User = require("../model/user");
+var Post = require("../model/post");
+var Comment = require("../model/comment");
+const GeneralHelper = require("../helper/general_helper");
+
+//Handle all requests with the pId parameter
+router.param("pId", function (req, res, next, id) {
+    Post.findById(id, function (err, doc) {
+        if (err) return next(err);
+        if (!doc) {
+            err = new Error("Not Found");
+            err.status = 404;
+            return next(err);
+        }
+        req.post = doc;
+        return next();
+    });
+});
+
+//Handle all requests with the cId parameter
+router.param("cId", function (req, res, next, id) {
+    Comment.findById(id, function (err, doc) {
+        if (err) return next(err);
+        if (!doc) {
+            err = new Error("Not Found");
+            err.status = 404;
+            return next(err);
+        }
+        req.comment = doc;
+        return next();
+    });
+});
+
+//GET /posts
+//Route for posts collection
+router.get("/", function (req, res, next) {
+    Post.find({})
+        .sort({ createdAt: -1 })
+        .exec(function (err, posts) {
+            if (err) return next(err);
+            res.json(posts);
+        });
+});
+
+//POST /posts
+//Route for creating posts
+router.post("/", function (req, res) {
+    if (GeneralHelper.validateParams(req, ["userId", "title", "body"])) {
+        var post = new Post(req.body);
+        post.save(function (err, post) {
+            if (err) return next(err);
+            res.status(201);
+            res.json(post);
+        });
+    } else {
+        var err = new Error("Parameter(s) missing");
+        err.status = 422;
+        return next(err);
+    }
+});
+
+//GET /posts/:pId
+//Route for specific post reading
+router.get("/:pId", function (req, res) {
+    res.json(req.post);
+});
+
+//PUT /posts/:pId
+//Route for specific post updating
+router.put("/:pId", function (req, res) {
+    req.post.update(req.body, function (err, result) {
+        if (err) return next(err);
+        res.json(result);
+    });
+});
+
+//DELETE /posts/:pId
+//Route for specific post deleting
+router.delete("/:pId", function (req, res) {
+    req.post.remove(function (err) {
+        if (err) return next(err);
+        res.json({
+            response: "Post was deleted"
+        });
+    });
+});
+
+//GET /posts/:pId/comments
+//Route for comments collection
+router.get("/:pId/comments", function (req, res) {
+    Comment.find({ postId: req.post.id })
+        .sort({ createdAt: -1 })
+        .exec(function (err, comments) {
+            if (err) return next(err);
+            res.json(comments);
+        });
+});
+
+//POST /posts/:pId/comments
+//Route for creating a comment
+router.post("/:pId/comments", function (req, res, next) {
+    if (GeneralHelper.validateParams(req, ["userId", "postId", "body"])) {
+        var comment = new Comment(req.body);
+        comment.save(function (err, comment) {
+            if (err) return next(err);
+            res.status(201);
+            res.json(comment);
+        });
+    } else {
+        var err = new Error("Parameter(s) missing");
+        err.status = 422;
+        return next(err);
+    }
+});
+
+//GET /posts/:pId/comments/:cId
+//Route for a specific comment reading
+router.get("/:pId/comments/:cId", function (req, res) {
+    res.json(req.comment);
+});
+
+//PUT /posts/:pId/comments/:cId
+//Route for a specific comment updating
+router.put("/:pId/comments/:cId", function (req, res) {
+    req.comment.update(req.body, function (err, result) {
+        if (err) return next(err);
+        res.json(result);
+    });
+});
+
+//DELETE /posts/:pId/comments/:cId
+//Route for a specific comment deleting
+router.delete("/:pId/comments/:cId", function (req, res) {
+    req.comment.remove(function (err) {
+        if (err) return next(err);
+        res.json({
+            response: "Comment was deleted"
+        });
+    });
+});
+
+module.exports = router;
